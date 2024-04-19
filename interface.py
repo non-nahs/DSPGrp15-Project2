1a3236e58d6f46632c2c74c940bfeece5876c55f
hyunsunryu2020 <HYUNSUN001@e.ntu.edu.sg>
2024-04-19 21:14:11 +0800

    Revert "Merge branch 'main' of https://github.com/non-nahs/DSPGrp15-Project2"
    This reverts commit af6eb08d0203728c3e4ac546c3b11de7bc670b11, reversing
    changes made to caf965b3ec856129d03376c623c6c76245dc4560.

--- interface.py
+++ interface.py
@@ -40,7 +40,6 @@ cost_params = {
     'cpu_hash_cost': 0.0025
 }
 
-
 class GraphNode(QGraphicsItem):
     def __init__(self, label, parent=None):
         super().__init__(parent)
@@ -101,25 +100,17 @@ class MainWindow(QMainWindow):
 
     def initUI(self):
         self.setWindowTitle("QEP Cost Estimator")
-        self.setGeometry(100, 100, 1600, 900)  # setGeometry(x, y, w, h)
+        self.setGeometry(100, 100, 1200, 800) # setGeometry(x, y, w, h)
         
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
         main_layout = QHBoxLayout(central_widget)
 
-        left_layout = QVBoxLayout()
-
         self.scene = QGraphicsScene()
         self.view = GraphicsView(self.scene)
         self.view.setRenderHint(QPainter.Antialiasing)  # For smoother graphics
-        self.view.setBackgroundBrush(QBrush(Qt.black))
-        left_layout.addWidget(self.view)
-
-        self.table_widget = QTableWidget()
-        self.table_widget.setEditTriggers(QTableWidget.NoEditTriggers)
-        self.table_widget.setAlternatingRowColors(True)
-        self.table_widget.setFixedHeight(180)
-        left_layout.addWidget(self.table_widget)
+        self.view.setBackgroundBrush(QBrush(Qt.black))  
+        main_layout.addWidget(self.view)
 
         right_layout = QVBoxLayout()
         
@@ -191,14 +182,12 @@ class MainWindow(QMainWindow):
 
         self.explain_label = QTextEdit("Query Execution Plan:")
         self.explain_label.setReadOnly(True)
-
-        self.explain_table = QTableWidget()
-        self.explain_table.setEditTriggers(QTableWidget.NoEditTriggers)
-
-        right_layout.addWidget(self.explain_table)
-
-        main_layout.addLayout(left_layout)
+        right_layout.addWidget(self.explain_label)
+        
         main_layout.addLayout(right_layout)
+        # self.result_display = QTextEdit()
+        # self.result_display.setReadOnly(True)
+        # layout.addWidget(self.result_display)
 
     def onLogin(self):
         if self.user_input.toPlainText().strip() == "" or self.host_input.toPlainText().strip() == "" or self.db_input.toPlainText().strip() == "" or self.port_input.toPlainText().strip() == "" or self.pwd_input.text().strip() == "":
@@ -228,13 +217,10 @@ class MainWindow(QMainWindow):
         sql_query = self.query_input.toPlainText().strip()
         plan = explain_query(sql_query)
 
+        # print(plan)
         self.showPlan(plan)
 
-        # self.display_explanation(plan)
-
-        self.update_result_table(sql_query)
-        self.update_explain_table(plan)
-
+        self.display_explanation(plan)
         # types = extract_node_types(json.loads(plan)[0]['Plan'])
 
         # for node_type in types:
@@ -253,15 +239,13 @@ class MainWindow(QMainWindow):
         plan = json.loads(plan)
         root_plan = plan[0]["Plan"]
         nodes = parse_plan(root_plan)
-
         for node in nodes:
             expected_cost = compute_expected_cost(node, cost_params)
             print(f"Node: {node['Node Type']}")
-            print(f"Expected Cost: {expected_cost:.4f}")
-            print(f"Actual Total Cost: {node['Total Cost']}")
-            print(f"Discrepancy: {node['Total Cost'] - expected_cost:.4f}\n")
-            output_str += f"Node type: \t{node['Node Type']}; Expected: {expected_cost:.4f}; Actual: {node['Total Cost']}; Discrepancy: {node['Total Cost'] - expected_cost:.4f}\n"
-
+            print(f"  Expected Cost: {expected_cost:.4f}")
+            print(f"  Actual Total Cost: {node['Total Cost']}")
+            print(f"  Discrepancy: {node['Total Cost'] - expected_cost:.4f}\n")
+            output_str += f"Node type: {node['Node Type']}; Expected Cost: {expected_cost:.4f}; Actual Total Cost: {node['Total Cost']}; Discrepancy: {node['Total Cost'] - expected_cost:.4f}\n"
         self.explain_label.setPlainText(output_str)
 
     def add_nodes_edges(self, node, parent_graphics_item=None, x=0, y=0, y_step=50, depth=0):
@@ -306,9 +290,7 @@ class MainWindow(QMainWindow):
 
         # Expand all nodes in the view for visibility
         self.tree_view.expandAll()
-
-        self.tree_view.resizeColumnToContents(0)
-
+        
     def load_nodes(self, node, parent_item):
         # Create an item with the node type
         node_item = QStandardItem(node['Node Type'])
@@ -319,6 +301,7 @@ class MainWindow(QMainWindow):
         # width_item = QStandardItem(f"{node['Plan Width']}")
         actual_time_item = QStandardItem(f"{node['Actual Total Time']}")
         
+        
         parent_item.appendRow([node_item, cost_item, planned_rows_item, actual_rows_item, actual_time_item])
         # parent_item.appendRow([node_item, cost_item, planned_rows_item, width_item, actual_time_item, actual_rows_item])
         
@@ -327,36 +310,13 @@ class MainWindow(QMainWindow):
             for subplan in node['Plans']:
                 self.load_nodes(subplan, node_item)
 
-    def update_result_table(self, sql_query):
-        sql_query_results = query_to_dataframe(sql_query)
 
-        self.table_widget.setRowCount(sql_query_results.shape[0])
-        self.table_widget.setColumnCount(sql_query_results.shape[1])
-        self.table_widget.setHorizontalHeaderLabels(sql_query_results.columns)
+def main():
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec())
 
-        for i in range(sql_query_results.shape[0]):
-            for j in range(sql_query_results.shape[1]):
-                self.table_widget.setItem(i, j, QTableWidgetItem(str(sql_query_results.iat[i, j])))
 
-        self.table_widget.resizeColumnsToContents()
-
-    def update_explain_table(self, plan):
-        self.explain_label.clear()
-
-        plan = json.loads(plan)
-        root_plan = plan[0]["Plan"]
-        nodes = parse_plan(root_plan)
-
-        self.explain_table.setRowCount(len(nodes))
-        self.explain_table.setColumnCount(4)
-        self.explain_table.setHorizontalHeaderLabels(['Node Type', 'Expected Cost', 'Actual Cost', 'Discrepancy'])
-
-        for i, node in enumerate(nodes):
-            expected_cost = compute_expected_cost(node, cost_params)
-
-            self.explain_table.setItem(i, 0, QTableWidgetItem(node['Node Type']))
-            self.explain_table.setItem(i, 1, QTableWidgetItem(f"{expected_cost:.4f}"))
-            self.explain_table.setItem(i, 2, QTableWidgetItem(f"{node['Total Cost']}"))
-            self.explain_table.setItem(i, 3, QTableWidgetItem(f"{node['Total Cost'] - expected_cost:.4f}"))
-
-        self.explain_table.resizeColumnsToContents()
+if __name__ == '__main__':
+    main()
